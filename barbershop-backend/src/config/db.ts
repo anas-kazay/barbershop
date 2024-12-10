5d80e278ca1174a6d49f2e0db352e3427d9d4ca5
anas-kazay <anaskazay@gmail.com>
2024-12-10 21:15:18 +0100

    modifiying db.ts

--- barbershop-backend/src/config/db.ts
+++ barbershop-backend/src/config/db.ts
@@ -6,6 +6,8 @@ dotenv.config();
 const mongoURI = process.env.MONGO_URI || "";
 
 const connectDB = async () => {
+  console.log("Connecting to MongoDB...");
+  console.log("mongoURI:", mongoURI);
   try {
     await mongoose.connect(mongoURI);
     console.log("MongoDB connected successfully");
